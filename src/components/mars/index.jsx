0c3c6b3ba43ad7eb8458702e5e2e7d295f54fe11
Axolottl <sanilogia@gmail.com>
2022-01-10 08:19:19 +0100

    Mars Rendered and finalized

--- src/components/mars/index.jsx
+++ src/components/mars/index.jsx
@@ -1,48 +1,88 @@
-import React from "react";
-import {useLoader} from "@react-three/fiber"
+import React, { useRef } from "react";
+import {useFrame, useLoader} from "@react-three/fiber"
 import { OrbitControls, Stars } from "@react-three/drei";
 import * as THREE from "three";
 
-import MarsDayMap from "../../assets/textures/5672_mars_4k_color.jpg";
+import MarsDayMap from "../../assets/textures/5672_marsmap4k.jpg";
 import MarsNormalMap from "../../assets/textures/5672_mars_4k_normal.jpg";
-import {TextureLoader} from "three";
+import MarsCloudMap from "../../assets/textures/mars_cloud_texture_by_aviacionvenezolana_ddneq0x-pre.png";
 
-export function Mars(props) {
+import { TextureLoader } from "three";
+import { TangentSpaceNormalMap } from "three";
+import { Camera } from "three";
 
-    const [colorMap, normalMap] = useLoader(TextureLoader, [MarsDayMap,MarsNormalMap]);
+export function Mars() {
+    const [colorMap, normalMap, cloudMap] = useLoader(TextureLoader, [MarsDayMap,MarsNormalMap, MarsCloudMap]);
+
+    const marsRef = useRef();
+    const cloud = useRef();
+    const meshRef = useRef();
+
+    useFrame(({ clock }) => {
+        const elapsedTime = clock.getElapsedTime();
+        marsRef.current.rotation.y = elapsedTime / 18;
+        cloud.current.rotation.y = elapsedTime / 9;
+        meshRef.current.metalness = (Math.cos(elapsedTime)+3)*5;
+    });
 
     return (
     <>
-        <ambientLight intensity={0.03} />
-        <spotLight 
-            intensity={1} 
-            position={[10,5,10]} />
+        <ambientLight intensity={0.001} />
+        <spotLight
+            distance={55}
+            castShadow={true}
+            color={"white"}
+            intensity={0.7} 
+            position={[30,-1,0]} />
+
         <Stars 
-            radius={300} 
-            depth={60} 
-            count={10000} 
+            radius={450} 
+            depth={100} 
+            count={8000} 
             factor={7} 
             saturation={0} 
             fade={true}/>
+
         <mesh>
             <sphereGeometry args={[3.02,32,32]} />
-            <meshPhongMaterial 
-                opacity={0.1} 
+            <meshStandardMaterial
+                opacity={0.2} 
                 depthWrite={true} 
                 transparent={true} 
-                color={"orange"} 
-                side={THREE.DoubleSide}>
-            </meshPhongMaterial>
+                metalness={2.4}
+                roughness={19}
+                color={"orange"}
+                side={THREE.DoubleSide}
+                />
         </mesh>
-        <mesh>
+        
+        <mesh ref={cloud}>
+            <sphereGeometry args={[3.04,32,32]} />
+            <meshStandardMaterial ref={meshRef}
+                map={cloudMap}
+                opacity={0.2}
+                depthWrite={true}
+                transparent={true}
+                roughness={1}
+            />
+        </mesh>
+        <mesh ref={marsRef}>
             <sphereGeometry args={[3,32,32]} />
-            <meshStandardMaterial 
-                map={colorMap} 
-                normalMap={normalMap} />
-            <OrbitControls 
+            <meshStandardMaterial
+                map={colorMap}
+                normalMap={normalMap}
+                normalMapType={TangentSpaceNormalMap}
+                normalScale={[0.2,0.2]}
+                metalness={0.1}
+                roughness={1}/>
+                
+            <OrbitControls
                 enableRotate={true} 
                 enablePan={true} 
                 enableZoom={true} 
+                minDistance={4.0}
+                maxDistance={20.0}
+                maxZoom={10.0}
                 zoomSpeed={0.6} 
                 panSpeed={0.5} 
                 rotateSpeed={0.4}/>
